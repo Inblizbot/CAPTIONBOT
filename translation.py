677aa67b7cc34edc3305171d014d7bff2264c53b
M U Z Z A F I R <97307696+TGEBOTZ@users.noreply.github.com>
2022-06-27 22:01:05 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,7 +8,7 @@ __I Am Auto CaptionBot Just Add In Channel and See Magic__
 
 **ɪ ᴄᴀɴ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴀᴅᴅ ᴘʀᴇ-sᴇᴛᴛᴇᴅ ᴄᴀᴘᴛɪᴏɴ ᴛᴏ ᴛʜᴇ ғɪʟᴇs ɪɴ ᴄʜᴀɴɴᴇʟs**
 
-__ɪ ᴀᴍ ᴄᴜʀʀᴇɴᴛʟʏ ᴡᴏʀᴋɪɴɢ ғᴏʀ ᴀ ᴄʜᴀɴɴᴇʟ ᴜsᴇ ᴍᴇ__
+__ɪ ᴀᴍ ᴄᴜʀʀᴇɴᴛʟʏ ᴡᴏʀᴋɪɴɢ __
 
 😈 __ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ__ 👉 __@{}__
 """    
@@ -33,33 +33,33 @@ __ɪ ᴀᴍ ᴄᴜʀʀᴇɴᴛʟʏ ᴡᴏʀᴋɪɴɢ ғᴏʀ ᴀ ᴄʜᴀɴɴᴇ
       ABOUT_TEXT = """
 📕 **𝐀𝐛𝐨𝐮𝐭 𝐌𝐞**
 
-__○ ᴍʏ ɴᴀᴍᴇ : [CapXbot](https://t.me/Avishkarpatil)__
+__○ ᴍʏ ɴᴀᴍᴇ : [CapXbot](https://t.me/inbliz)__
 __○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ __
 __○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ __
 __○ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ __
 __○ ᴠᴇʀsɪᴏɴ : 2.0.1__
-__○ ᴄʀᴇᴀᴛᴏʀ :  @AvishkarPatil__
+__○ ᴄʀᴇᴀᴛᴏʀ : [⃯⃯⃑⃑ɪ ɴ ʙ ꪸʟꪲꪳ𐺬𑀸 ɪ ᴢ⃡ꪲ꪿ꪷꪰꪸ🇮🇳](https://t.me/inbliz)__
  
-**[© ᴀᴠɪsʜᴋᴀʀ ᴘᴀᴛɪʟ](https://t.me/Avishkarpatil)**
+**[© ⃯⃯⃑⃑ɪ ɴ ʙ ꪸʟꪲꪳ𐺬𑀸 ɪ ᴢ⃡ꪲ꪿ꪷꪰꪸ🇮🇳](https://t.me/inbliz)**
 """
 
       MARKDOWN_TEXT = """
 🔰 <u>𝐀𝐛𝐨𝐮𝐭 𝐌𝐚𝐫𝐤𝐝𝐨𝐰𝐧</u>
 👉 <b>Bold text</b>
-🔸 <code>**Avishkar**</code>
+🔸 <code>**⃯⃯⃑⃑ɪ ɴ ʙ ꪸʟꪲꪳ𐺬𑀸 ɪ ᴢ⃡ꪲ꪿ꪷꪰꪸ🇮🇳**</code>
 
 👉 <b>Italic text</b>
-🔹 <code>__Avishkat__</code> 
+🔹 <code>__⃯⃯⃑⃑ɪ ɴ ʙ ꪸʟꪲꪳ𐺬𑀸 ɪ ᴢ⃡ꪲ꪿ꪷꪰꪸ🇮🇳__</code> 
 
 👉 <b>Code text</b>
-🔸 <code>`Avishkar`</code>   
+🔸 <code>`⃯⃯⃑⃑ɪ ɴ ʙ ꪸʟꪲꪳ𐺬𑀸 ɪ ᴢ⃡ꪲ꪿ꪷꪰꪸ🇮🇳`</code>   
 
 👉 <b>Hyperlink text</b>
-🔹 <code>[hyperlink_text](https://avipatilweb.me)</code> 
+🔹 <code>[hyperlink_text](https://t.me/inbliz)</code> 
 
 〰〰〰〰〰〰〰〰〰〰
 
-<b><a href="https://t.me/Avishkarpatil">© ᴀᴠɪsʜᴋᴀʀ ᴘᴀᴛɪʟ</a></b>
+<b><a href="https://t.me/inbliz">© M U Z Z A F I R</a></b>
 """
 
 # Bot status display
@@ -72,7 +72,7 @@ __○ ᴄʀᴇᴀᴛᴏʀ :  @AvishkarPatil__
 
 📐 <b>Current Position :</b> {}
 
-<b><a href="https://t.me/Avishkarpatil">© ᴀᴠɪsʜᴋᴀʀ ᴘᴀᴛɪʟ</a></b>
+<b><a href="https://t.me/inbliz">© ⃯⃯⃑⃑ɪ ɴ ʙ ꪸʟꪲꪳ𐺬𑀸 ɪ ᴢ⃡ꪲ꪿ꪷꪰꪸ🇮🇳</a></b>
 """
 
 
@@ -81,7 +81,7 @@ __○ ᴄʀᴇᴀᴛᴏʀ :  @AvishkarPatil__
 ○ <b> I Am Available Open Source on Github 
       Click Below Link And Deploy Me Now </b>
 
-○ <i>DEPLOY</i> : <b><a href="https://heroku.com/deploy?template=https://github.com/avipatilpro/Caption-Bot">On Heroku</a></b>    
+○ <i>DEPLOY</i> : <b><a href="https://heroku.com/deploy?template=https://github.com/TGEBOTZ/CAPTIONBOT">On Heroku</a></b>    
 
-○ <i>SOURCE</i> : <b><a href="https://github.com/avipatilpro/Caption-Bot">Caption Bot</a></b>  
+○ <i>SOURCE</i> : <b><a href="https://github.com/TGEBOTZ/CAPTIONBOT">Caption Bot</a></b>  
 """
